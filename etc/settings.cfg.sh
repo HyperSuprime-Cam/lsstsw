74ef447c2a2441a9f8a5632abb05a23075b69d50
Paul Price <price@astro.princeton.edu>
2017-11-09 11:08:32 +0800

    point to HSC repos

--- etc/settings.cfg.sh
+++ etc/settings.cfg.sh
@@ -19,7 +19,7 @@ VERSIONDB_PUSH=${VERSIONDB_PUSH:-false}
 LSSTSW=$(cd "$(dirname "$BASH_SOURCE")/.."; pwd)
 
 # the location of source repositories
-REPOSFILE_REPO=${REPOSFILE_REPO:-lsst/repos}
+REPOSFILE_REPO=${REPOSFILE_REPO:-HyperSuprime-Cam/repos}
 REPOSFILE=${REPOSFILE:-${LSSTSW}/etc/repos.yaml}
 
 # location of the build directory
